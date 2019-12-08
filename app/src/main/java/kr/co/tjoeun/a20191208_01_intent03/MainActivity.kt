d20ec468e2a0b2589c6de9d42c9aeb1033a0871a
blessme333 <blessme333@gmail.com>
2019-12-08 10:25:37 +0900

    이름 입력화면을 입력용으로 호출

--- app/src/main/java/kr/co/tjoeun/a20191208_01_intent03/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/a20191208_01_intent03/MainActivity.kt
@@ -1,12 +1,31 @@
 package kr.co.tjoeun.a20191208_01_intent03
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+//    1000이라는 숫자만 보면 어떤 의미로 쓰는 값인지 알기 어렵다.
+//    상수로 이름을 지어줘서 읽기 편하게 가공하는게 좋다.
+    val REQ_CODE_FOR_INPUT_NAME = 1000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        inputNameBtn.setOnLongClickListener {
+            val intent = Intent(this, EditnameActivity::class.java)
+//            startActivity(intent)  => 단순 화면 이동을 위한 호출
+
+//            결과를 받아오기 위한 "요청(Request)"을 하면서 화면 이동.
+//            어떤 요청인지 구별해주는 숫자값을 적어줘야 한다.
+//            숫자는 나중에 뭔지 알아보기 힘들어서 맨 윗줄에 val로 이름을 만들어준다.
+            startActivityForResult(intent, REQ_CODE_FOR_INPUT_NAME)
+        }
+
+
     }
 }
