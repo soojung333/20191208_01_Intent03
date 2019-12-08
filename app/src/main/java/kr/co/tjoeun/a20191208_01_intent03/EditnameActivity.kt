cac0d545aeb532e3e116ab68b3c72efcbc1c83d1
blessme333 <blessme333@gmail.com>
2019-12-08 10:16:12 +0900

    메인화면 그리기

---
+++ app/src/main/java/kr/co/tjoeun/a20191208_01_intent03/EditnameActivity.kt
@@ -0,0 +1,12 @@
+package kr.co.tjoeun.a20191208_01_intent03
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditnameActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_editname)
+    }
+}
